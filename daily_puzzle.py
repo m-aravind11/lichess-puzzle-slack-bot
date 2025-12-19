9e10d309464efa322e1aeaa6d25035892991112b
Aravind <maravind2000@gmail.com>
2025-12-19 18:29:48 +0530

    Add FastAPI endpoints for submit and sendPuzzle

--- daily_puzzle.py
+++ daily_puzzle.py
@@ -9,17 +9,21 @@ from slack_sdk.errors import SlackApiError
 
 class Constants:
     LICHESS_DAILY_PUZZLE_URL = "https://lichess.org/api/puzzle/daily"
+    LICHESS_PUZZLE_SOLUTION_URL = "https://lichess.org/api/puzzle/"
     CHESSVISION_FEN_TO_IMAGE_URL = "https://fen2image.chessvision.ai/"
     PUZZLE_IMAGE_FILENAME_TEMPLATE = "Lichess Daily Puzzle {}.png"
 
 class LichessDailyPuzzle:
     def __init__(self):
-        self.LICHESS_OAUTH_TOKEN = os.environ['LICHESS_APP_OAUTH_TOKEN']
-        self.SLACK_CHANNEL_ID = os.environ['SLACK_CHANNEL_ID']
+        self.LICHESS_OAUTH_TOKEN = "xoxb-458414646086-10111224817207-uPdYAu7kdTD1tBrFD00yNX7c"
+        self.SLACK_CHANNEL_ID = "C0A4113U1CZ"
         
     def get_lichess_daily_puzzle(self) -> dict:
         return requests.get(Constants.LICHESS_DAILY_PUZZLE_URL).json()
     
+    def get_solution(self, puzzle_id:str) -> list:
+        return requests.get(Constants.LICHESS_PUZZLE_SOLUTION_URL+puzzle_id).json()['puzzle']['solution']
+         
     def get_pgn_from_daily_puzzle(self,daily_puzzle: dict) -> str:
         return daily_puzzle['game']['pgn']
 
@@ -60,6 +64,7 @@ class LichessDailyPuzzle:
                 channel=self.SLACK_CHANNEL_ID, 
                 file=filepath, 
                 initial_comment="{} to play".format(self.whose_move(board).upper()))
+            print (response)
             assert response["file"]  # the uploaded file
         except SlackApiError as e:
             # You will get a SlackApiError if "ok" is False
@@ -67,7 +72,7 @@ class LichessDailyPuzzle:
             assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
             print(f"Got an error: {e.response['error']}")
 
-    def handle_puzzle_generation_and_sending(self) -> None:
+    async def handle_puzzle_generation_and_sending(self) -> None:
         pgn = self.get_pgn_from_daily_puzzle(self.get_lichess_daily_puzzle())
         fen = self.get_fen_from_pgn(pgn)
         encoded_fen = self.encode_fen_for_url(fen)
@@ -76,7 +81,3 @@ class LichessDailyPuzzle:
 
         self.save_puzzle_image(self.get_image_link_from_fen(encoded_fen), self.puzzle_filename)
         self.send_puzzle_to_slack(self.get_board_from_fen(fen))
-
-if __name__ == "__main__":
-    obj=LichessDailyPuzzle()
-    obj.handle_puzzle_generation_and_sending()
---
+++ handle_response.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from daily_puzzle import LichessDailyPuzzle
+
+class Submission(BaseModel):
+    lichess_puzzle_id: str
+    user_id: str
+    moves: str
+    
+app = FastAPI()
+lichess = LichessDailyPuzzle()
+
+@app.get('/')
+async def root():
+    return {"Message": "Hello"}
+
+@app.post('/submit')
+async def submit_response(submission: Submission) -> bool:
+    moves=submission.moves.split(' ')
+    solution=lichess.get_solution(submission.lichess_puzzle_id)
+    return True if moves==solution else False 
+
+@app.post('/send_puzzle')
+async def send_daily_puzzle():
+    await lichess.handle_puzzle_generation_and_sending()
+    return "Ok"
